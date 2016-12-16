f0e1be9d5afb6e31efc46b7c1a6d7e9aa035a3d9
qbsonn <jkrasowski2@gmail.com>
2016-12-16 21:02:09 +0100

    Dodano miejsce na Tester ;p

---
+++ Tester/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Kuba on 2016-12-16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
