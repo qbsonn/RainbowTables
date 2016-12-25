4e84c1ac300a0825c10dafe5309c85cd22788077
karwacia <karwacia@gmail.com>
2016-12-26 00:05:19 +0100

    Dodanie jakiegos bruteforca (jeszcze dokładnie nie sprawdzalam jak dziala )

--- Tester/src/Tester/Main.java
+++ Tester/src/Tester/Main.java
@@ -6,6 +6,11 @@ package Tester;
 public class Main {
     public static void main(String[] args) {
 
-        Test test=new Test();
+       // Test test=new Test();
+        new Bruteforce() {
+            public void element(char[] result, int offset, int length) {
+                System.out.println(new String(result, offset, length));
+            }
+        }.generate("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     }
 }
