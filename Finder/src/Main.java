b815be10ddb5a77a25d9a5dcd17e084993206c59
qbsonn <jkrasowski2@gmail.com>
2016-12-26 18:14:26 +0100

    Dodanie miejsca na Finder

---
+++ Finder/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kuba on 2016-12-26.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+
+}
