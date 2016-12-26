fa27434b6c1cf814c32bb2712662cd64c726d611
karwacia <karwacia@gmail.com>
2016-12-26 21:16:09 +0100

    dodano hashowanie haseł

--- Tester/src/Tester/Main.java
+++ Tester/src/Tester/Main.java
@@ -1,5 +1,7 @@
 package Tester;
 
+import java.util.ArrayList;
+
 /**
  * Created by Patrycja on 2016-12-22.
  */
@@ -7,11 +9,19 @@ public class Main {
     public static void main(String[] args) {
 
         // Test test=new Test();
+       HashAndReduct hashreduct = new HashAndReduct("MD5", "ABCDEFGHIJKLMNOPRSTUVWXYZ");
         Bruteforce bruteforce = new Bruteforce();
+        ArrayList <byte[]> bytesList=new ArrayList<>();
+
 
         for (int length = bruteforce.min; length <= bruteforce.max; length++) // Change bruteforce.min and bruteforce.max for number of characters to bruteforce.
             bruteforce.generate("", 0, length); //prepend_string, pos, length
 
+
+        for(int j=0;j<bruteforce.combinations.size();j++)
+        { bytesList.add(hashreduct.calculateHash(bruteforce.combinations.get(j).getBytes())) ;
+        System.out.println(bytesList.get(j));
+        }
         bruteforce.saveToFile("pliczek2");
     }
 }
